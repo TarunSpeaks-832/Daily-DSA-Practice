232a711d563820a64c1132513834013474a21288
Mallula Tarun Kumar <tarunkumarm832@gmail.com>
2026-07-16 12:07:09 +0530

    Time: 3 ms (87.55%), Space: 47.7 MB (51.25%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candy[] = new int[n];
+        
+        candy[0] = 1;
+        for(int i=1;i<n;i++){
+            candy[i] = 1;
+            if(ratings[i] > ratings[i-1]){
+                candy[i] = candy[i-1] + 1;
+            }
+        }
+        
+        int sum = candy[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                if(candy[i] <= candy[i+1]) candy[i] = candy[i+1] + 1 ;
+            }
+            sum += candy[i];
+        }
+        return sum;
+    }
+}
