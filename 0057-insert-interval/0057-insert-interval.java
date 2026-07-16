28c1f226bb664ecdc839534b998e9e50f7a5f257
Mallula Tarun Kumar <tarunkumarm832@gmail.com>
2026-07-16 22:26:13 +0530

    Time: 1 ms (98.04%), Space: 47 MB (75.37%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,38 +1,27 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int i=0;
         int n = intervals.length;
-        int[][] arr = new int[n+1][2];
-        for(int i=0;i<n;i++){
-            arr[i][0] = intervals[i][0];
-            arr[i][1] = intervals[i][1];
+        ArrayList<int[]> res = new ArrayList<>();
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
-        arr[n][0] = newInterval[0];
-        arr[n][1] = newInterval[1];
 
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int st = arr[0][0];
-        int end = arr[0][1];
-
-        List<int[]> result = new ArrayList<>();
-
-        for(int i=1;i<n+1;i++){
-            if(arr[i][0] > end){
-                result.add(new int[]{st, end});
-                st = arr[i][0];
-                end = arr[i][1];
-            }
-            else{
-                end = Math.max(end, arr[i][1]);
-            }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        
-        result.add(new int[]{st, end});
 
-        return result.toArray(new int[result.size()][]);
+        res.add(newInterval);
+
+        while(i < n){
+            res.add(intervals[i]);
+            i++;
+        }
+
+        return res.toArray(new int[res.size()][]);
     }
 }
